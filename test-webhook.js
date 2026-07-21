0c8c2da3509c12dff25961aa3c5917e4564469dd
Dibyanshu <ddibyanshu2@gmail.com>
2026-07-21 15:41:55 +0530

    Clean up unnecessary test scripts

--- test-webhook.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(`[Webhook Received] ${req.method} ${req.url}`);
-  res.writeHead(200);
-  res.end("OK");
-});
-server.listen(4000, () => console.log("Webhook test server listening on port 4000"));
